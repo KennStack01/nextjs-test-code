94f98ca9b37006e24f57ef68b8e26420d149f2af
Kenn Stack <kibadikennedy@gmail.com>
2021-08-06 20:11:02 +0800

    first n last commit

---
+++ pages/about.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Head from 'next/head'
+
+const About = () => {
+    return (
+        <>
+            <Head>
+                <title>Ninja List | About</title>
+                <meta name="description" content="Ninjas" />
+            </Head>
+            <div>
+                <h1>About 👋</h1>
+                <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae delectus culpa aut dolorem suscipit voluptate beatae earum error. Veritatis reiciendis fuga velit et obcaecati possimus hic voluptatibus tenetur eius recusandae. </p>
+                <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae delectus culpa aut dolorem suscipit voluptate beatae earum error. Veritatis reiciendis fuga velit et obcaecati possimus hic voluptatibus tenetur eius recusandae. </p>
+            </div>
+        </>
+    )
+}
+
+export default About
